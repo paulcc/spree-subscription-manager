04e436b4e35525a3266af00e6a3ffe20e15aeb2b
paulcc <paulcc.two@gmail.com>
2009-03-31 13:06:15 +0100

    added basic email addr checking by creating a fake user
    (this makes use of verification in the authlogic gem)

--- app/controllers/mailing_lists_controller.rb
+++ app/controllers/mailing_lists_controller.rb
@@ -1,7 +1,17 @@
 class MailingListsController < ApplicationController
  
   def join_list
-    render :inline => "request noted"
+    if params[:signup] && params[:signup][:email]
+      fake_user = User.new
+      fake_user.password = fake_user.password_confirmation = "1234"
+      fake_user.email = params[:signup][:email]
+      if fake_user.valid?
+        flash[:notice] = "Thank you. We will add <strong>#{params[:signup][:email]}</strong> to the offers newsletter."
+      else
+        flash[:notice] = "Address not added. It did not look like a valid address. Please try again."
+      end
+    end
+    redirect_to :back
   end 
 
 =begin  
