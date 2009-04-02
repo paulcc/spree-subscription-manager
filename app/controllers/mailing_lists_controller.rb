1491245531234ba27387ac5e1ace68194815177d
paulcc <paulcc.two@gmail.com>
2009-04-02 09:42:01 +0100

    generalised - message and list come from form now

--- app/controllers/mailing_lists_controller.rb
+++ app/controllers/mailing_lists_controller.rb
@@ -1,18 +1,20 @@
 class MailingListsController < ApplicationController
   before_filter :require_no_user, :only => [:blank_join_list]
- 
+
+  # used to add dummy user to a specified list 
   def blank_join_list
     if params[:signup] && params[:signup][:email]
       pw = Digest::SHA1.hexdigest("--#{Time.now.to_s}#{self.object_id}#{Array.new(256){rand(256)}.join}")
       user = User.new
       user.password = user.password_confirmation = pw
       user.email = params[:signup][:email]
-      user.name = user.email 
+      user.name = user.email # only by default 
 
       if user.save_without_session_maintenance	# prevents auto-login
-        flash[:notice] = "Thank you! We will add <strong>#{params[:signup][:email]}</strong> to the offers newsletter. You are now signed up to receive our exclusive offers by email."
+        #flash[:notice] = "We will add <strong>#{params[:signup][:email]}</strong> to the #{params[:signup][:list]} newsletter. #{params[:signup][:welcome]}"
+        flash[:notice] = params[:signup][:welcome] 
         user.roles << Role.find_by_name("mail_list")
-        user.mailing_lists << MailingList.find_by_name("Promotions")
+        user.mailing_lists << MailingList.find_by_name(params[:signup][:list])
       else
         flash[:notice] = "Email address not added. It did not look like a valid address, or has already been used. Please try again."
       end
